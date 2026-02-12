03784a03617c219d66d72b96509bc5269bf2924e
Frans Wallin <frans.wallin@rjl.se>
2026-02-12 13:29:04 +0100

    uploaded script.

---
+++ README.md
@@ -0,0 +1,5 @@
+#du_cleaner
+WANRING: Will remove files. Use at own risk. 
+The script will remove directories in the target directory if they are old enough.
+#quick start
+./du_cleaner.sh [target_directory]
---
+++ du_cleaner.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+# quit script if any of the commands fails. Note that && commands should be in parentheses for this to work.
+set -eo pipefail
+trap 'exit_status="$?" && echo Failed on line: $LINENO at command: $BASH_COMMAND && echo "exit status $exit_status"' ERR
+
+# The directory that contain subdirectories. The dubdirectories will be removed if old enough.
+target="$1"
+
+# Thresholds
+# If the available % is lower than this (or same), the cleaning starts.
+perc_min=20
+# If the available GB is lower (or same) than this, the cleaning starts.
+gb_min=190
+
+read tot used avai perc <<< $(df -BG . | awk 'NR==2 {print $2, $3, $4, $5}')
+
+# Remove units
+tot_gb=${tot%G}
+avai_gb=${avai%G}
+perc_used=${perc%\%}
+
+echo $tot_gb
+echo $avai_gb
+echo $perc_used
+
+# Calculate free percentage
+free_perc=$((100 - $perc_used))
+
+echo "Total: ${tot_gb}G"
+echo "Free: ${avai_gb}G (${free_perc}% free)"
+
+# Check conditions. Remove iname if we want all patterns in target.
+if [ "$free_perc" -le "$perc_min" ] || [ "$avai_gb" -le "$gb_min" ]; then
+    date
+    echo "Low space detected. Will try to remove directories in $target"
+    find "$target" -mindepth 1 -maxdepth 1 -type d -iname 'run_*' -mtime +14 -print  -exec rm -rf {} +
+fi
+
+echo "end of script"
